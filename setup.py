c930c00f4f4c10bcad39946e12db35ddd9d5c4da
Sri Ramesh Naidu <rockstar.rn71@gmail.com>
2025-04-14 05:23:36 +0100

    update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=open('requirements.txt').read().splitlines(),
     entry_points={
         'console_scripts': [
-            'vortex = vortex.main:main',
+            'vorteX = vortex.main:main',
         ],
     },
 )
